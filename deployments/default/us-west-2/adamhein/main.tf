59dc7eeeff3b744161281260007e17952b22a7ef
Casey Reed <cpcj79@yahoo.com>
2022-05-15 17:24:25 -0500

    KAZI: new server

---
+++ deployments/default/us-west-2/adamhein/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+locals {
+  tags = {
+    AccountingCategory = "CaseyNAdam"
+    Service            = "adamheim"
+  }
+  app_name = "adamheim"
+}
+
+
+module "app" {
+  source = "../../../../modules/app"
+
+  app_name = local.app_name
+
+  load_balancer_type = "network"
+  user_data          = file("${path.module}/user_data.sh")
+
+  lb_port = 19999
+  lb_proto = "TCP"
+
+  app_port  = 2456
+  app_proto = "UDP"
+
+  app0_port  = 2457
+  app0_proto = "UDP"
+
+  app1_port  = 2458
+  app1_proto = "UDP"
+
+  app2_port  = 19999
+  app2_proto = "TCP"
+
+  enable_efs = true
+
+  # ASG 
+  min_instance = 1
+  max_instance = 1
+}
+
+resource "aws_ssm_parameter" "world_name" {
+  name = "/app/${local.app_name}/world_name"
+  type = "String"
+  value = local.app_name
+}
+
+resource "aws_ssm_parameter" "world_password" {
+  name = "/app/${local.app_name}/world_password"
+  type = "SecureString"
+  value = "butts"
+
+}
---
+++ deployments/default/us-west-2/frankenheim/user_data.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -xe
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+set -e
+
+export AWS_DEFAULT_REGION=us-west-2
+
+useradd -m valheim
+sudo yum install -y glibc.i686 libstdc++.i686 SDL2 amazon-efs-utils
+
+cd /home/valheim/
+mkdir -p /home/valheim/.config/
+sudo mount -t efs -o tls,accesspoint=fsap-061ee2b59be49fb07,iam fs-074f544272d503693:/ /home/valheim/.config
+
+mkdir -p valheim-server/ 
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+chown -R valheim:valheim /home/valheim/
+
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
+systemctl restart network.service
+
+sudo su valheim
+
+linux64 /home/valheim/steamcmd.sh +force_install_dir valheim-server/ +login anonymous +app_update 896660 validate +quit
+
+export HOME=/home/valheim
+export SteamAppId=892970
+export templdpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+
+
+/home/valheim/valheim-server/valheim_server.x86_64 -name "valheim" -port 2456 -world "valheim" -password "butts" -batchmode -nographics -public 1
+
+export LD_LIBRARY_PATH=$templdpath
