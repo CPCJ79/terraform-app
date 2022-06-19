6a82ead7315e7c8823555ecc1133c0f9f770b9b2
Casey Reed <cpcj79@yahoo.com>
2022-06-19 10:20:21 -0500

    KAZI: ok ok ok... docker it is

--- deployments/default/us-east-2/frising/user_data.sh
+++ deployments/default/us-east-2/frising/user_data.sh
@@ -9,25 +9,30 @@ export EFS_ACCESS_POINT_ID=$(aws ssm get-parameter --name /app/frising/efs_acces
 export EFS_FILESYSTEM_POINT_ID=$(aws ssm get-parameter --name /app/frising/efs_fs_id --query Parameter.Value --output text)
 
 useradd -m vrising
-sudo yum install -y glibc.i686 libstdc++.i686 SDL2 amazon-efs-utils --setopt=protected_multilib=false epel-release wine xorg-x11-server-Xvfb
+sudo yum -y docker
+
+systemctl start docker
 
 cd /home/vrising/
-mkdir -p /home/vrising/VRising
-sudo mount -t efs -o tls,accesspoint=${EFS_ACCESS_POINT_ID},iam ${EFS_FILESYSTEM_POINT_ID}:/ /home/vrising/VRising
+mkdir -p /home/vrising/persistentdata
+sudo mount -t efs -o tls,accesspoint=${EFS_ACCESS_POINT_ID},iam ${EFS_FILESYSTEM_POINT_ID}:/ /home/vrising/persistentdata
 
-curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
 chown -R vrising:vrising /home/vrising/
 
 sudo su vrising
-
-linux64 /home/vrising/steamcmd.sh +force_install_dir /home/vrising/ +login anonymous +app_update 1829350 validate +quit
 
 export HOME=/home/vrising
 export SteamAppId=1829350
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
-export WINEARCH=win64
 
-mkdir -p save-data/Settings
+docker run -d --name='vrising' \
+--net='bridge' \
+-e TZ="America/New_York" \
+-e SERVERNAME="frising" \
+-v '/home/vrising':'/mnt/vrising/server':'rw' \
+-v '/home/vrising/persistentdata':'/mnt/vrising/persistentdata':'rw' \
+-p 9876:9876/udp \
+-p 9877:9877/udp \
+'trueosiris/vrising'
 
-/usr/bin/xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' /usr/bin/wine VRisingServer.exe -persistentDataPath ./save-data -logFile server.log
